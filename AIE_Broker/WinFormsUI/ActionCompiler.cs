9be057bd5f7a86102390dcb3316b719b05076141
Robert Henry <rhenry74@hotmail.com>
2024-02-17 04:40:30 -0500

    parse parameter

--- AIE_Broker/WinFormsUI/ActionCompiler.cs
+++ AIE_Broker/WinFormsUI/ActionCompiler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AIE_InterThread;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +22,10 @@ namespace WinFormsUI
 
         public bool Compiled { get; set; } = false;
         public bool Compiling { get; set; } = false;
+        public ApplicationCapibility CapibilityToExecute { get; private set; }
+        public string Error { get; private set; }
 
-        
+        public string Parameter = null;       
 
         public void Compile()
         {
@@ -29,15 +33,58 @@ namespace WinFormsUI
 
             Task.Run(async () =>
             {
-                Program.SharedContext.AutomationLog.Enqueue("Compiling: " + this.ActionText);
-
-                var embedding = await Embedding.GetForAsync(this.ActionText);
-
-                var possibleActions = await Embedding.TopThreeCapibilitiesFor(embedding);
-                Program.SharedContext.AutomationLog.Enqueue("Top 3:");
-                foreach (var possibleAction in possibleActions)
+                try
                 {
-                    Program.SharedContext.AutomationLog.Enqueue(possibleAction.Action);
+                    Program.SharedContext.AutomationLog.Enqueue("Compiling: " + this.ActionText);
+
+                    //strip out parameter text
+                    string actionText = this.ActionText;
+                    var startPosition = this.ActionText.IndexOf('[');
+                    if (startPosition > -1)
+                    {
+                        Program.SharedContext.AutomationLog.Enqueue("Parsing parameter...");
+                        var endPosition = this.ActionText.Substring(startPosition + 1).IndexOf("]");
+                        this.Parameter = this.ActionText.Substring(startPosition + 1, startPosition + endPosition - startPosition);
+                        actionText = this.ActionText.Substring(0, startPosition);
+                        actionText = actionText + "?";
+                        if (this.ActionText.Length > startPosition + endPosition)
+                        {
+                            actionText = actionText + this.ActionText.Substring(startPosition + endPosition + 2,
+                                this.ActionText.Length - (startPosition + endPosition) - 2);
+                        }
+                    }
+
+                    //find best match
+                    var embedding = await Embedding.GetForAsync(actionText);
+
+                    var comparisons = await Embedding.TopThreeCapibilitiesFor(embedding);
+                    Program.SharedContext.AutomationLog.Enqueue("Top 3:");
+                    EmbeddingComparison topChoice = null;
+                    foreach (var comparison in comparisons)
+                    {
+                        Program.SharedContext.AutomationLog.Enqueue(comparison.Capibility.Action + " - " +
+                            comparison.DotProduct);
+                        if (topChoice == null)
+                        {
+                            topChoice = comparison;
+                        }
+                    }
+
+                    if (topChoice.DotProduct > 0.97)
+                    {
+                        this.CapibilityToExecute = topChoice.Capibility;
+                        Program.SharedContext.AutomationLog.Enqueue("Compilation Success.");
+                    }
+                    else
+                    {
+                        this.Error = "Ambigous";
+                        Program.SharedContext.AutomationLog.Enqueue(this.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Program.SharedContext.AutomationLog.Enqueue(ex.ToString());
+                    this.Error = ex.ToString();
                 }
 
                 Compiled = true;
